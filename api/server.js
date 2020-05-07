ddff6932729a7e52d4abfcdcfda84f213afabe3b
janecyyu <janecyyu@gmail.com>
2020-05-07 09:59:21 -0700

    add config

--- api/server.js
+++ api/server.js
@@ -7,6 +7,10 @@ const server = express();
 
 server.use(helmet());
 
+server.get('/',function(req,res){
+    res.status(200).json({env:process.env.NODE.ENV})
+})
+
 server.use('/api', apiRouter);
 
 module.exports = server;
